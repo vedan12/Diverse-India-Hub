2c928bb40893dc92e8db8f03c9e17c8a853ff7d6
vedan12 <156008040+vedan12@users.noreply.github.com>
2024-03-20 08:14:37 +0530

    Structural part of project has been done

---
+++ script.js
@@ -0,0 +1,33 @@
+function goToCostumes() {
+    window.location.href = 'costumes.html';
+}
+function goToFood() {
+    window.location.href = 'food.html';
+}
+function goToSports() {
+    window.location.href = 'sports.html';
+}
+function goToFestivals() {
+    window.location.href = 'festivals.html';
+}
+function goToLanguages() {
+    window.location.href = 'languages.html';
+}
+function goToTourism() {
+    window.location.href = 'tourism.html';
+}
+function goToCities() {
+    window.location.href = 'cities.html';
+}
+function goToDances() {
+    window.location.href = 'dances.html';
+}
+function goToPolitics() {
+    window.location.href = 'politics.html';
+}
+function goToStates() {
+    window.location.href = 'states.html';
+}
+function goToSignUp() {
+    window.location.href = 'signup.html';
+}
