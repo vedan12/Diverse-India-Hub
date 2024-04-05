26a6e3c0fd7d15ff2ca342bf6ee12b1bcc7bf0e0
vedan12 <156008040+vedan12@users.noreply.github.com>
2024-04-05 10:54:14 +0530

    miniproject part 2

--- script.js
+++ script.js
@@ -28,6 +28,27 @@ function goToPolitics() {
 function goToStates() {
     window.location.href = 'states.html';
 }
+function goToSongs() {
+    window.location.href = '';
+}
+function goToReligions() {
+    window.location.href = 'religions.html';
+}
+function goToFeedback() {
+    window.location.href = 'feedback.html';
+}
 function goToSignUp() {
     window.location.href = 'signup.html';
 }
+function goToLogin() {
+    window.location.href = 'login.html';
+} 
+function goToAboutUs() {
+    window.location.href = 'about-us.html';
+} 
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
